863d3bbd676c229df64c2d40bcef83a0ae6f8b85
Jamie from Desktop <jamiebclark@gmail.com>
2014-09-23 16:12:44 -0400

    Updated schema

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -9,9 +9,9 @@ class TalkBackSchema extends CakeSchema {
 	}
 
 	public $tb_channel_commenter_admins = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'commenter_id_2' => array('column' => array('commenter_id', 'channel_id'), 'unique' => 1),
@@ -22,7 +22,7 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_channels = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'prefix' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 64, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
@@ -30,8 +30,8 @@ class TalkBackSchema extends CakeSchema {
 		'allow_topics' => array('type' => 'boolean', 'null' => true, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'forum_count' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'topic_count' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'forum_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'topic_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'prefix' => array('column' => 'prefix', 'unique' => 0)
@@ -40,9 +40,9 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_channels_commenter_types = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'commenter_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'commenter_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'channel_id_2' => array('column' => array('channel_id', 'commenter_type_id'), 'unique' => 1),
@@ -53,9 +53,9 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_channels_commenters = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'channel_id_2' => array('column' => array('channel_id', 'commenter_id'), 'unique' => 1),
@@ -66,9 +66,9 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_commenter_email_controls = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'comment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'comment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
 		'email_on_reply' => array('type' => 'boolean', 'null' => true, 'default' => '1'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
@@ -80,10 +80,10 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_commenter_has_reads = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'model' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 128, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
@@ -93,23 +93,10 @@ class TalkBackSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
 	);
 
-	public $tb_commenter_types_commenters = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'commenter_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'commenter_type_id_2' => array('column' => array('commenter_type_id', 'commenter_id'), 'unique' => 1),
-			'commenter_type_id' => array('column' => 'commenter_type_id', 'unique' => 0),
-			'commenter_id' => array('column' => 'commenter_id', 'unique' => 0)
-		),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
-	);
-
 	public $tb_commenters_messages = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'message_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'message_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'commenter_id_2' => array('column' => array('commenter_id', 'message_id'), 'unique' => 1),
@@ -120,15 +107,15 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_comments = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'lft' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'rght' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'depth' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'lft' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'rght' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'depth' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'model' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 128, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'prefix' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 32, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
@@ -144,12 +131,13 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_forums = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'channel_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
 		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'topic_count' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'private' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
+		'topic_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
@@ -159,15 +147,41 @@ class TalkBackSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
 	);
 
+	public $tb_forums_commenter_types = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'forum_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'commenter_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'forum_id' => array('column' => array('forum_id', 'commenter_type_id'), 'unique' => 1),
+			'forum_id_2' => array('column' => 'forum_id', 'unique' => 0),
+			'commenter_type_id' => array('column' => 'commenter_type_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $tb_forums_commenters = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'forum_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'forum_id' => array('column' => array('forum_id', 'commenter_id'), 'unique' => 1),
+			'forum_id_2' => array('column' => 'forum_id', 'unique' => 0),
+			'commenter_id' => array('column' => 'commenter_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
 	public $tb_messages = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'from_commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'last_commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'from_commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'last_commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'subject' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 6),
-		'first_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'last_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 6, 'unsigned' => false),
+		'first_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'last_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'viewed' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
@@ -178,16 +192,16 @@ class TalkBackSchema extends CakeSchema {
 	);
 
 	public $tb_topics = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'forum_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
-		'first_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'last_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'commenter_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'forum_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'key' => 'index'),
+		'first_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'last_comment_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'locked' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
 		'sticky' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
 		'indexes' => array(
